703604568217aa9a8f9a29bf4429464a2a2d657b
Max Chernakov <41rm4kc@gmail.com>
2022-06-04 12:55:36 +0300

    #000030 - update advice item view

--- source/feature/main/advices/src/main/kotlin/dev/draftine/advices/presentation/adapter/AdviceRecyclerItem.kt
+++ source/feature/main/advices/src/main/kotlin/dev/draftine/advices/presentation/adapter/AdviceRecyclerItem.kt
@@ -1,24 +1,19 @@
 package dev.draftine.advices.presentation.adapter
 
-import android.view.Gravity
+import android.view.View
 import dev.draftine.advices.R
 import dev.draftine.advices.presentation.model.AdviceModel
 import dev.draftine.ui.recycler.Item
 import dev.draftine.ui.recycler.RecyclerItem
-import dev.draftine.ui.text.TextType
 import dev.draftine.ui.text.TextView
 
-class AdviceRecyclerItem : RecyclerItem<TextView, AdviceModel>() {
+class AdviceRecyclerItem : RecyclerItem<View, AdviceModel>() {
 
     override val layoutId = R.layout.advice_item
 
     override fun isRelativeItem(item: Item) = item is AdviceModel
 
-    override fun bind(view: TextView, item: AdviceModel) {
-        view.apply {
-            gravity = Gravity.CENTER
-            applyTextType(TextType.HEADLINE_6)
-            text = item.text
-        }
+    override fun bind(view: View, item: AdviceModel) {
+        view.findViewById<TextView>(R.id.advice_item_text).text = item.text
     }
 }
